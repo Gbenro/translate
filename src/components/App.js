6b78ce0fd4615da07763e2eed8a1f0696f82741a
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-21 14:04:40 -0600

    Added color context

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import UserCreate from './UserCreate'
 import LanguageContext from '../contexts/LanguageContext'
+import ColorContext from '../contexts/ColorContext'
 
 export default class App extends Component {
   state = {
@@ -24,9 +25,11 @@ export default class App extends Component {
             onClick={() => this.onLanguageChange('dutch')}
           />
         </div>
-        <LanguageContext.Provider value={this.state.language}>
-          <UserCreate />
-        </LanguageContext.Provider>
+        <ColorContext.Provider value='red'>
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
       </div>
     )
   }
