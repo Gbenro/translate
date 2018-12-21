afe2120339c9348abdda1583cff251a20f4fefc1
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-21 13:07:10 -0600

    aaded UserCreate

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
+  state = {
+    language: 'english'
+  }
+
+  onLanguageChange = language => {
+    this.setState({ language })
+  }
   render () {
-    return <div />
+    return (
+      <div className='ui container'>
+        <div>
+          Select a language:
+          <i
+            className=' flag us'
+            onClick={() => this.onLanguageChange('english')}
+          />
+          <i
+            className='flag nl'
+            onClick={() => this.onLanguageChange('dutch')}
+          />
+        </div>
+        {this.state.language}
+      </div>
+    )
   }
 }
